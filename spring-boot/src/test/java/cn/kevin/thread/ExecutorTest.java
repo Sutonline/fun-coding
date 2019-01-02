5324dcd807d1c863d2a2b04ed4854479526c1c7a
sutonline <sutonlineskyon@gmail.com>
2019-01-02 18:36:12 +0800

    对于Executor的线程不销毁，那么线程的上下文也不会清空，所以同一个线程在执行下一个任务的时候可能会被其他先前线程设置的上下文所影响

---
+++ spring-boot/src/test/java/cn/kevin/thread/ExecutorTest.java
@@ -0,0 +1,49 @@
+package cn.kevin.thread;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author yongkang.zhang
+ * created at 2019-01-02
+ */
+@Slf4j
+public class ExecutorTest {
+
+	@Test
+	public void test() throws InterruptedException {
+		ThreadFactoryBuilder builder = new ThreadFactoryBuilder();
+		builder.setNameFormat("exec-%d");
+		ExecutorService executor = new ThreadPoolExecutor(2, 2, 300, TimeUnit.SECONDS, new ArrayBlockingQueue<>(1000));
+
+		for (int i = 0; i < 3; i++) {
+			executor.submit(() -> {
+				Person person = new Person();
+				person.set();
+				log.info("输出结果: {}", person.get());
+			});
+		}
+
+		Thread.sleep(1000 * 60);
+		executor.shutdown();
+	}
+
+
+	private static class Person {
+		public final static ThreadLocal<Integer> threadLocal = ThreadLocal.withInitial(() -> 1);
+
+		public void set() {
+			threadLocal.set(threadLocal.get() + 1);
+		}
+
+		public Integer get() {
+			return threadLocal.get();
+		}
+	}
+}
