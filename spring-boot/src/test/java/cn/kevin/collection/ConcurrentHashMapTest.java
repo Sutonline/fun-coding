d4c6056905ebd8751a6609726ee715c0592cce66
sutonline <sutonlineskyon@gmail.com>
2019-05-23 04:56:16 +0800

    ConcurrentHashMap和Hashtable测试

---
+++ spring-boot/src/test/java/cn/kevin/collection/ConcurrentHashMapTest.java
@@ -0,0 +1,48 @@
+package cn.kevin.collection;
+
+import org.junit.Test;
+
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * ConcurrentHashMap和Hashtable的key,value都不允许null元素
+ *
+ * @author yongkang.zhang
+ * created at 2019-05-23
+ */
+public class ConcurrentHashMapTest {
+
+	@Test
+	public void mapTest() {
+		Map<String, String> map = new ConcurrentHashMap<>();
+		try {
+			map.put(null, "我是value");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		try {
+			map.put("key", null);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	@Test
+	public void hashTableTest() {
+		Hashtable<String, String> hashtable = new Hashtable<>();
+		try {
+			hashtable.put(null, "value");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		try {
+			hashtable.put("key", null);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
